4f1d681fedfe9ac656ee0c534386fb6d33b24b70
Romain Lorrilliere <romainlorrilliere@gmail.com>
2018-10-17 01:12:41 +0200

    analysis_modif_prev

--- analysis.r
+++ analysis.r
@@ -5,7 +5,7 @@
 
 
 
-habitat_specilisation<- function(dataBrut=FALSE,fileAnalysis="data/analysisHabiat.csv") {
+habitat_prevalence_winter <- function(dataBrut=FALSE,fileAnalysis="data/analysisHabiat.csv") {
     require(reshape2)
     require(ggplot2)
     require(data.table)
@@ -15,7 +15,7 @@ habitat_specilisation<- function(dataBrut=FALSE,fileAnalysis="data/analysisHabia
     if(dataBrut) {
         d <- read.delim("data/export_shoc_04072018_155413.txt",stringsAsFactors=FALSE,header=TRUE,encoding="UTF-8")
 
-        dd <- subset(d,d$Distance.de.contact %in% c("LESS25","LESS100","LESS200","U")& N..Passage > 0,select=c("Code.inventaire","N..Carré.EPS","Date","N..Passage","EXPORT_STOC_TEXT_EPS_POINT","Espèce","Nombre","EPS.P.Milieu"))
+        dd <- subset(d,d$Distance.de.contact %in% c("LESS25","LESS100","LESS200","U")& N..Passage >0 ,select=c("Code.inventaire","N..Carré.EPS","Date","N..Passage","EXPORT_STOC_TEXT_EPS_POINT","Espèce","Nombre","EPS.P.Milieu"))
 
         dd$Espèce[dd$Espèce==""] <- "OTHERS"
         dd$Espèce <- substr(dd$Espèce,1,6)
@@ -104,7 +104,7 @@ habitat_specilisation<- function(dataBrut=FALSE,fileAnalysis="data/analysisHabia
     ddsp_hab <- merge(ddsp_hab,ddsp,by="sp")
     ddsp_hab <- merge(ddsp_hab,thab,by="hab")
 
-ddsp_hab$prevalence <- (ddsp_hab$ab/ddsp_hab$ab_tot)*(ddsp_hab$pr/ddsp_hab$hab_nb)
+    ddsp_hab$prevalence <- (ddsp_hab$ab/ddsp_hab$ab_tot)*(ddsp_hab$pr/ddsp_hab$hab_nb)
 
 
     dprevalence <- dcast(sp ~habitat_name,data=ddsp_hab,value.var="prevalence")
@@ -260,10 +260,10 @@ ddSpe_prop.agg <- merge(ddSpe_prop.agg,tsp,by.x="sp",by.y="pk_species")
     dprev_wide <- dcast(ddSpe.agg2,sp~habitat_name,value.var="mean")
     colnames(dprev_wide) <- c("pk_species","shoc_farmland_prev_index","shoc_woodland_prev_index","shoc_urban_prev_index")
 
-    tindic <- read.csv2("../Birdlab/generic_data/espece_list_indicateur.csv",encoding="UTF-8")
-    tindic <- tindic[,c(1:15,19:21)]
-    tindic$habitat_specialisation_f <- gsub("Ã¢","â",tindic$habitat_specialisation_f)
-    colnames(tindic)[16:18] <- c("shoc_farmland_prev","shoc_woodland_prev","shoc_urban_prev")
+    tindic <- read.csv("../Birdlab/generic_data/espece_list_indicateur.csv",encoding="UTF-8")
+##    tindic <- tindic[,c(1:15,19:21)]
+##    tindic$habitat_specialisation_f <- gsub("Ã¢","â",tindic$habitat_specialisation_f)
+##    colnames(tindic)[16:18] <- c("shoc_farmland_prev","shoc_woodland_prev","shoc_urban_prev")
     dim(tindic)
     tindic <- merge(tindic,dprev_wide,by="pk_species",all.x=TRUE)
     dim(tindic)
@@ -271,11 +271,277 @@ ddSpe_prop.agg <- merge(ddSpe_prop.agg,tsp,by.x="sp",by.y="pk_species")
     tindic$shoc_woodland_prev <- ifelse(is.na(tindic$shoc_woodland_prev_index),FALSE,tindic$shoc_woodland_prev_index > 0)
     tindic$shoc_urban_prev <- ifelse(is.na(tindic$shoc_urban_prev_index),FALSE,tindic$shoc_urban_prev_index > 0)
 
-    tindic <- tindic[,1:18]
+    tindic <- tindic[,sort(setdiff(1:ncol(tindic),grep("index",colnames(tindic))))]
 
     write.csv(tindic,"../Birdlab/generic_data/espece_list_indicateur.csv",na="",row.names=FALSE,fileEncoding="UTF-8")
 
-    tindex <- read.csv("../Birdlab/generic_data/espece_list_indicateur - Copie (4).csv",encoding="UTF-8")
+    tindex <- read.csv("../Birdlab/generic_data/espece_indicateur_fonctionel.csv",encoding="UTF-8")
+
+    tindex <- merge(tindex,dprev_wide,by="pk_species",all=TRUE)
+
+    write.csv(tindex,"../Birdlab/generic_data/espece_indicateur_fonctionel.csv",na="",row.names=FALSE,fileEncoding="UTF-8")
+
+
+}
+
+habitat_prevalence_breeding <- function(dataBrut=FALSE,fileAnalysis="data/analysisHabiat_breeding.csv") {
+    require(reshape2)
+    require(ggplot2)
+    require(data.table)
+
+    dataBrut=FALSE
+    fileAnalysis="data/analysisHabiatBreeding.csv"
+    if(dataBrut) {
+        d <- read.csv2("data/data_FrenchBBS_point_romain_allSp_2014_2017fr.csv",stringsAsFactors=FALSE)
+
+        dd <- subset(d,select=c("point","carre","annee","id_point_annee","code_sp","abondance","habitat_principal"))
+
+        dd$code_sp[dd$code_sp==""] <- "OTHERS"
+        dd$code_sp<- substr(dd$code_sp,1,6)
+
+
+        ddan_w <- dcast(dd,point + carre + annee + id_point_annee + habitat_principal ~ code_sp,value.var="abondance")
+
+        ddan_w[is.na(ddan_w)] <- 0
+
+
+
+
+        ddan2 <- melt(ddan_w,id.vars=c("point","carre","annee","id_point_annee","habitat_principal"))
+        colnames(ddan2)[6:7] <- c("sp","ab")
+
+
+
+
+        ddcarre <- aggregate(ab~carre +annee + sp,ddan2,sum)
+        colnames(ddcarre)<- c("carre","annee","sp","ab_carre")
+        ddcarre <- subset(ddcarre,ab_carre>0)
+
+
+        ddpoint <- merge(ddan2,ddcarre,by=c("carre","annee","sp"))
+        ddpoint$log_ab <- log(ddpoint$ab+1)
+        ddpoint$carrepoint <- paste(ddpoint$carre,ddpoint$point)
+
+
+        ddhab <- unique(dd[,c("carre","point","habitat_principal")])
+        colnames(ddhab) <- c("carre","point","hab")
+        ddhab$carrepoint <- paste(ddhab$carre,ddhab$point)
+
+        habdupli <- which(duplicated(ddhab$carrepoint))
+
+        ddhab <- ddhab[!(1:nrow(ddhab)%in%habdupli),]
+
+        ddhab <- ddhab[,c("carrepoint","hab")]
+
+        ddpoint <- merge(ddpoint,ddhab,by="carrepoint")
+
+        write.csv(ddpoint,fileAnalysis,row.names=FALSE)
+    } else {
+
+        ddpoint <- read.csv(fileAnalysis)
+    }
+
+    thab <- read.csv("data/habitat.csv")
+
+    dodo <-  unique(ddpoint[,c("carrepoint","hab")])
+    dodo <- merge(dodo,thab,by="hab")
+
+    gghab <- ggplot(dodo,aes(x=factor(1),fill=habitat_name))+ geom_bar(width = 1)+
+  coord_polar("y")
+
+    ggsave("output/habitatSHOC.png",gghab)
+
+
+    ## Prevalence indicator
+    ##    Andrade, C., Chiron, F., Julliard, R., 2012. Improving the selection of focal species exposed to pesticides to support ecological risk assessments. Ecotoxicology 21, 2430–2440. https://doi.org/10.1007/s10646-012-0982-4
+
+
+    ddpoint$pr <- as.numeric(ddpoint$ab>0)
+
+    sp_pr <- aggregate(pr~sp,ddpoint,sum)
+    sp_pr <- subset(sp_pr, pr>200)
+
+    ddpoint <- subset(ddpoint,sp %in% sp_pr$sp)
+
+    ddhab <- as.data.frame(table(ddpoint$hab))
+    colnames(ddhab) <- c("hab","hab_nb")
+
+    ddsp <- aggregate(ab~sp,ddpoint,sum)
+    colnames(ddsp) <- c("sp","ab_tot")
+
+    ddsp_hab <- aggregate(cbind(ddpoint$ab,ddpoint$pr)~hab+sp,ddpoint,sum)
+    colnames(ddsp_hab) <- c("hab","sp","ab","pr")
+
+    ddsp_hab <- merge(ddsp_hab,ddhab,by="hab")
+    ddsp_hab <- merge(ddsp_hab,ddsp,by="sp")
+    ddsp_hab <- merge(ddsp_hab,thab,by="hab")
+
+    ddsp_hab$prevalence <- (ddsp_hab$ab/ddsp_hab$ab_tot)*(ddsp_hab$pr/ddsp_hab$hab_nb)
+
+
+    dprevalence <- dcast(sp ~habitat_name,data=ddsp_hab,value.var="prevalence")
+    colnames(dprevalence)[c(4,7,8)] <- c("woodland","urban","water")
+
+    tsp <- read.csv2("data/espece_list_indicateur.csv",encoding="UTF-8")
+    tsp <- tsp[,c("pk_species","french_name","english_name","birdlab","birdlab_shoc_prevalence_farmland","birdlab_shoc_prevalence_woodland","birdlab_shoc_prevalence_urban")]
+
+    dprevalence <- merge(dprevalence,tsp,by.x="sp",by.y="pk_species")
+    require(ggtern)
+
+    gg <- ggtern(subset(dprevalence,birdlab),aes(farmland,woodland,urban,label=sp,color=birdlab_shoc_prevalence_farmland))+geom_text(size=2)
+gg
+
+### resampling 300 by main habitat
+    dodo$grHab <- ifelse(dodo$habitat_name %in% c("farmland","forest","urban area"),as.character(dodo$habitat_name),"other")
+
+        dddprev <- NULL
+        dddprev_prop <- NULL
+
+    for(i in 1:400){
+
+        dodo_<- dodo[sample(1:nrow(dodo)),]
+        dodo.dt <- data.table(dodo_)
+        dodo.dt <- dodo.dt[,order_gr := 1:.N, by=grHab]
+        dodo2 <- as.data.frame(dodo.dt)
+
+        dodo2 <- subset(dodo2,order_gr <= 300)
+        ## gghab <- ggplot(dodo2,aes(x=factor(1),fill=habitat_name))+ geom_bar(width = 1)+
+        ## coord_polar("y")
+        ##  ggsave("output/habitatSHOC_resampled.png",gghab)
+
+
+        ddpoint2 <- subset(ddpoint,carrepoint %in% dodo2$carrepoint)
+
+        ddsp <- aggregate(ab~sp,ddpoint2,sum)
+        colnames(ddsp) <- c("sp","ab_tot")
+
+        ddsp_hab <- aggregate(ab ~ hab+sp,ddpoint2,sum)
+        colnames(ddsp_hab) <- c("hab","sp","ab")
+
+        ddsp_hab <- merge(ddsp_hab,ddsp,by="sp")
+
+        ddsp_hab$prevalence <- (ddsp_hab$ab/ddsp_hab$ab_tot)
+        ddsp_hab <- merge(ddsp_hab,thab,by="hab")
+
+        dprevalence <- dcast(sp ~habitat_name,data=ddsp_hab,value.var="prevalence")
+        colnames(dprevalence)[c(4,7,8)] <- c("woodland","urban","water")
+        dprevalence[is.na(dprevalence)] <- 0
+
+        dprevalence <- subset(dprevalence,(dprevalence$farmland + dprevalence$woodland + dprevalence$urban)>0)
+        dprevalence$sum3hab <- (dprevalence$farmland + dprevalence$woodland + dprevalence$urban)
+     #   dprevalence <- subset(dprevalence,sum3hab>.75)
+        dprevalence$farmland_prop <- dprevalence$farmland / (dprevalence$farmland + dprevalence$woodland + dprevalence$urban)
+        dprevalence$woodland_prop <- dprevalence$woodland / (dprevalence$farmland + dprevalence$woodland + dprevalence$urban)
+        dprevalence$urban_prop <- dprevalence$urban / (dprevalence$farmland + dprevalence$woodland + dprevalence$urban)
+
+        dprev_prop <- dprevalence[,c("sp","farmland_prop","woodland_prop","urban_prop")]
+        colnames(dprev_prop) <- c("sp","farmland","woodland","urban")
+        dprev <- dprevalence[,1:(ncol(dprevalence)-4)]
+
+        ddprev <- melt(id.vars="sp",data=dprev)
+        colnames(ddprev)[2:3] <- c("habitat_name","preval")
+        ddprev$preval <- ddprev$preval - 0.3
+        ddprev$idset <- i
+        ddprev_prop <- melt(id.vars="sp",data=dprev_prop)
+        colnames(ddprev_prop)[2:3] <- c("habitat_name","preval_prop")
+        ddprev_prop$idset <- i
+
+
+
+
+        dddprev <- rbind(dddprev,ddprev)
+        dddprev_prop<- rbind(dddprev_prop,ddprev_prop)
+    }
+
+    ddSpe.nb <- aggregate(preval~sp+habitat_name,dddprev,length)
+
+    ddSpe.agg <- aggregate(preval~sp+habitat_name,dddprev,quantile,c(0.025,0.5,0.975))
+
+    ddSpe.agg <- data.frame(ddSpe.agg[,1:2],ddSpe.agg[,3])
+    colnames(ddSpe.agg)[3:5] <- c("ICinf","med","ICsup")
+
+    ddSpe.agg.mean <- aggregate(preval~sp+habitat_name,dddprev,mean)
+    colnames(ddSpe.agg.mean)[3] <- c("mean")
+
+    ddSpe.agg <- merge(ddSpe.agg,ddSpe.agg.mean,by = c("sp","habitat_name"))
+
+    ddSpe_prop.agg <- aggregate(preval_prop~sp+habitat_name,dddprev_prop,quantile,c(0.025,0.5,0.975))
+
+    ddSpe_prop.agg <- data.frame(ddSpe_prop.agg[,1:2],ddSpe_prop.agg[,3])
+    colnames(ddSpe_prop.agg)[3:5] <- c("ICinf","med","ICsup")
+
+    ddSpe_prop.agg.mean <- aggregate(preval~sp+habitat_name,dddprev,mean)
+    colnames(ddSpe_prop.agg.mean)[3] <- c("mean")
+
+    ddSpe_prop.agg <- merge(ddSpe_prop.agg,ddSpe_prop.agg.mean,by = c("sp","habitat_name"))
+
+
+ddSpe.agg <- merge(ddSpe.agg,tsp,by.x="sp",by.y="pk_species")
+ddSpe_prop.agg <- merge(ddSpe_prop.agg,tsp,by.x="sp",by.y="pk_species")
+
+
+
+     dres.bl <- subset(ddSpe.agg,birdlab & habitat_name%in% c("farmland","woodland","urban"))
+    vecCol <- c("farmland"="#ffb319","woodland"="#1a9129","urban"="#9439c4")
+
+
+     ttsp <- read.csv("data/espece.csv",encoding="UTF-8")
+    ttsp <- subset(ttsp,select=c("pk_species","euring","french_name","english_name"))
+    colnames(ttsp)[1] <- "sp"
+
+
+    orderSp <- subset(ttsp,sp %in% dres.bl$sp)
+    dres.bl$english_name <- factor(dres.bl$english_name, levels = orderSp$english_name)
+
+
+
+    gg <- ggplot(dres.bl,aes(x=english_name,y=mean,group=habitat_name,fill=habitat_name,colour=habitat_name))
+    gg <- gg + geom_hline(yintercept=0,colour="white",size=2)
+    gg <- gg + geom_bar(stat="identity", position="dodge",width=.7,alpha=.5)
+    gg <- gg + geom_hline(yintercept=0,colour="white",size=2,alpha=.5)
+    gg <- gg + geom_errorbar(aes(ymin=ICinf,ymax=ICsup),position = position_dodge(),width=.7 )
+    gg <- gg + geom_errorbar(aes(ymin=med,ymax=med),position = position_dodge(),width=.7 )
+    gg <- gg + theme(axis.text.x = element_text(angle = 45, hjust = 1))+ labs(x="",y="Habitat prevalence",fill="Habitat",colour="Habitat")
+    gg <- gg + scale_fill_manual(values=vecCol)+scale_colour_manual(values=vecCol)
+    gg
+    ggsave("output/breeding_prevalence_habitat.png",gg)
+
+
+
+    gg <- ggplot(subset(dres.bl,mean>0),aes(x=english_name,y=mean,group=habitat_name,fill=habitat_name,colour=habitat_name))
+    gg <- gg + geom_hline(yintercept=0,colour="white",size=2)
+    gg <- gg + geom_bar(stat="identity", position="dodge",width=.7,alpha=.5)
+    gg <- gg + geom_hline(yintercept=0,colour="white",size=2,alpha=.5)
+    gg <- gg + theme(axis.text.x = element_text(angle = 45, hjust = 1))+ labs(x="",y="Habitat prevalence",fill="Habitat",colour="Habitat")
+    gg <- gg + scale_fill_manual(values=vecCol)+scale_colour_manual(values=vecCol)
+    gg
+   ggsave("output/breeding_prevalence_habitat_sup0.png",gg)
+
+
+    ddSpe.agg <- ddSpe.agg[,1:8]
+
+    write.csv(ddSpe.agg,"output/stoc_prevalence.csv",row.names=FALSE)
+
+    ddSpe.agg2 <- subset(ddSpe.agg,habitat_name %in% c("farmland","woodland","urban"))
+    dprev_wide <- dcast(ddSpe.agg2,sp~habitat_name,value.var="mean")
+    colnames(dprev_wide) <- c("pk_species","stoc_farmland_prev_index","stoc_woodland_prev_index","stoc_urban_prev_index")
+
+    tindic <- read.csv("../Birdlab/generic_data/espece_list_indicateur.csv",encoding="UTF-8")
+   # tindic <- tindic[,c(1:15,19:21)]
+   # tindic$habitat_specialisation_f <- gsub("Ã¢","â",tindic$habitat_specialisation_f)
+   # colnames(tindic)[16:18] <- c("shoc_farmland_prev","shoc_woodland_prev","shoc_urban_prev")
+    dim(tindic)
+    tindic <- merge(tindic,dprev_wide,by="pk_species",all.x=TRUE)
+    dim(tindic)
+    tindic$stoc_farmland_prev <- ifelse(is.na(tindic$stoc_farmland_prev_index),FALSE,tindic$stoc_farmland_prev_index > 0)
+    tindic$stoc_woodland_prev <- ifelse(is.na(tindic$stoc_woodland_prev_index),FALSE,tindic$stoc_woodland_prev_index > 0)
+    tindic$stoc_urban_prev <- ifelse(is.na(tindic$stoc_urban_prev_index),FALSE,tindic$stoc_urban_prev_index > 0)
+
+    tindic <- tindic[,sort(setdiff(1:ncol(tindic),grep("index",colnames(tindic))))]
+
+    write.csv(tindic,"../Birdlab/generic_data/espece_list_indicateur.csv",na="",row.names=FALSE,fileEncoding="UTF-8")
+
+    tindex <- read.csv("../Birdlab/generic_data/espece_indicateur_fonctionel.csv",encoding="UTF-8")
 
     tindex <- merge(tindex,dprev_wide,by="pk_species",all=TRUE)
 
@@ -286,6 +552,72 @@ ddSpe_prop.agg <- merge(ddSpe_prop.agg,tsp,by.x="sp",by.y="pk_species")
 
 
 
+
+analysis_modif_prev <- function() {
+    tindex <- read.csv("../Birdlab/generic_data/espece_indicateur_fonctionel.csv",encoding="UTF-8",stringsAsFactors=FALSE)
+
+    ttind <- tindex[,c(1,grep("prev",colnames(tindex)))]
+    ttind <- na.omit(ttind)
+
+    tindic <- read.csv("../Birdlab/generic_data/espece_list_indicateur.csv",encoding="UTF-8",stringsAsFactors=FALSE)
+    tindic <- tindic[,c("pk_species","habitat_specialisation")]
+    tindic$habitat_specialisation[tindic$habitat_specialisation==""] <- "any"
+    ttind <- merge(ttind,tindic,by="pk_species")
+
+
+    ttsp <- read.csv("data/espece.csv",encoding="UTF-8",stringsAsFactors=FALSE)
+    ttsp <- subset(ttsp,select=c("pk_species","euring","french_name","english_name"))
+
+    ttind <- merge(ttind,ttsp,by="pk_species")
+
+    orderSp <- subset(ttsp,pk_species %in% ttind$pk_species)
+    ttind$english_name <- factor(ttind$english_name, levels = orderSp$english_name)
+
+    ggfarmland <- melt(ttind[,c("pk_species","english_name","stoc_farmland_prev_index","shoc_farmland_prev_index")],id.vars=c("pk_species","english_name"))
+    colnames(ggfarmland)[4] <- "prevalence_value"
+    ggfarmland$season <- "winter"
+    ggfarmland$season[grep("stoc",ggfarmland$variable)] <- "breeding"
+    ggfarmland <- merge(ggfarmland,tindic,by="pk_species")
+    ggfarmland$prevalence_name <- "farmland"
+
+ggwoodland <- melt(ttind[,c("pk_species","english_name","stoc_woodland_prev_index","shoc_woodland_prev_index")],id.vars=c("pk_species","english_name"))
+    colnames(ggwoodland)[4] <- "prevalence_value"
+    ggwoodland$season <- "winter"
+    ggwoodland$season[grep("stoc",ggwoodland$variable)] <- "breeding"
+    ggwoodland <- merge(ggwoodland,tindic,by="pk_species")
+    ggwoodland$prevalence_name<- "woodland"
+
+ggurban <- melt(ttind[,c("pk_species","english_name","stoc_urban_prev_index","shoc_urban_prev_index")],id.vars=c("pk_species","english_name"))
+    colnames(ggurban)[4] <- "prevalence_value"
+    ggurban$season <- "winter"
+    ggurban$season[grep("stoc",ggurban$variable)] <- "breeding"
+    ggurban <- merge(ggurban,tindic,by="pk_species")
+    ggurban$prevalence_name<- "urban"
+
+
+
+      vecCol <- c("farmland"="#ffb319","woodland"="#1a9129","urban"="#ff0404","generalist"="#1200ff","any"="black")
+
+    ggtab <- rbind(rbind(ggfarmland,ggwoodland),ggurban)
+
+    tsp_dec <- data.frame(pk_species=unique(ggtab$pk_species),
+                          sp_decal=runif(length(unique(ggtab$pk_species)),0,4))
+    ggtab <- merge(ggtab,tsp_dec,by="pk_species")
+    ggtab$text_hjust <- ifelse(ggtab$season=="breeding",1.5 + ggtab$sp_dec ,-.5 - ggtab$sp_dec)
+
+    gg <- ggplot(ggtab,aes(x=season,y=prevalence_value,color=habitat_specialisation,group=pk_species,label=pk_species)) + facet_grid(prevalence_name~.)
+    gg <- gg + geom_hline(yintercept=0,size=2,color="white")
+    gg <- gg + geom_point()+geom_line()
+    gg <- gg + geom_hline(yintercept=0,size=2,color="white",alpha=.25)
+    gg <- gg + geom_text(aes(hjust=text_hjust),size=2.5)
+    gg <- gg + labs(x="season",y="Farmland prevalence",color="Indicator groups")
+    gg <- gg + scale_colour_manual(values=vecCol)
+
+    gg
+
+    ggsave("output/prevalence_breeding_winter.png",gg,height=12,width=12)
+
+}
 
 
 
